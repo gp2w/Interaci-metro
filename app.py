74a6276b97d7437b0a8261f7e9e1fb0a874d23e5
Pedro Nascimento <pedrovictorcosta7@gmail.com>
2020-08-07 22:14:08 -0300

    [WIP] ajustes

--- app.py
+++ app.py
@@ -23,13 +23,8 @@ auth.set_access_token(os.environ['ACCESS_TOKEN'],
                       os.environ['ACCESS_TOKEN_SECRET'])
 api = tweepy.API(auth)
 
-# tweets = core_cd.get_tweets(api=api, username='weversonvn')
-# likes = core_cd.get_likes(api=api, username='weversonvn')
-# inicializa dataframe vazio
-df = core_pd.top_users_likes(likes=[])
-
-df['id'] = df['user']
-df.set_index('id', inplace=True, drop=False)
+# inicializando dataframe
+df = pd.DataFrame(columns=['id','user','count'])
 
 # inicializa uma aplicacao em Dash
 app = dash.Dash(__name__, title='Interaciômetro')
@@ -48,21 +43,20 @@ app.layout = html.Div([
     dash_table.DataTable(
         id='datatable-row-ids',
         columns=[
-            {'name': i, 'id': i, 'deletable': False} for i in df.columns
-            # omit the id and index column
-            if i != 'id' and i != 'index'            
+            {'name': 'Usuário', 'id': 'user'},
+            {'name': 'Quantidade de Likes', 'id': 'count'}
         ],
         data=df.to_dict('records'),
-        editable=False,
+        # editable=False,
         filter_action="native",
         sort_action="native",
         sort_mode='multi',
-        row_selectable='multi',
-        row_deletable=False,
-        selected_rows=[],
+        # row_selectable='multi',
+        # row_deletable=True,
+        # selected_rows=[],
         page_action='native',
         page_current= 0,
-        page_size= 10,
+        page_size= 15,
     ),
     html.Div(id='datatable-row-ids-container')
 ])
@@ -71,78 +65,53 @@ app.layout = html.Div([
               [Input('submit-button-state', 'n_clicks')],
               [State('user-input', 'value')])
 def update_username(n_clicks, username):
-    # apaga dataframe anterior
-    df = core_pd.top_users_likes(likes=[])
+    # reseta dataframe
+    df = pd.DataFrame(columns=['id','user','count'])
+    
     if username != '':
-
         # tweets = core_cd.get_tweets(api=api, username='weversonvn')
         likes = core_cd.get_likes(api=api, username=username)
-
         df = core_pd.top_users_likes(likes=likes)
-
-        df['id'] = df['user']
-        df.set_index('id', inplace=True, drop=False)
+        # df['id'] = df['user']
+        df.set_index('user', inplace=True, drop=False)
 
     return df.to_dict('records')
 
 @app.callback(
     Output('datatable-row-ids-container', 'children'),
-    [Input('datatable-row-ids', 'derived_virtual_row_ids'),
-     Input('datatable-row-ids', 'selected_row_ids'),
-     Input('datatable-row-ids', 'active_cell')])
-def update_graphs(row_ids, selected_row_ids, active_cell):
-    # When the table is first rendered, `derived_virtual_data` and
-    # `derived_virtual_selected_rows` will be `None`. This is due to an
-    # idiosyncracy in Dash (unsupplied properties are always None and Dash
-    # calls the dependent callbacks when the component is first rendered).
-    # So, if `rows` is `None`, then the component was just rendered
-    # and its value will be the same as the component's dataframe.
-    # Instead of setting `None` in here, you could also set
-    # `derived_virtual_data=df.to_rows('dict')` when you initialize
-    # the component.
-    selected_id_set = set(selected_row_ids or [])
+    [Input('datatable-row-ids', 'derived_virtual_data')])
+def update_graphs(rows):
 
-    if row_ids is None:
-        dff = df
-        # pandas Series works enough like a list for this to be OK
-        row_ids = df['id']
+    if rows is None:
+        dff = pd.DataFrame(columns=['id','user','count'])
     else:
-        dff = df.loc[row_ids]
+        dff = pd.DataFrame(rows)
 
-    active_row_id = active_cell['row_id'] if active_cell else None
-
-    colors = ['#FF69B4' if id == active_row_id
-              else '#7FDBFF' if id in selected_id_set
-              else '#0074D9'
-              for id in row_ids]
-
+    print(dff)
     return [
         dcc.Graph(
-            id=column + '--row-ids',
+            id='likes',
             figure={
                 'data': [
                     {
-                        'x': dff['user'],
-                        'y': dff[column],
+                        "x": dff['user'],
+                        "y": dff['count'],
                         'type': 'bar',
-                        'marker': {'color': colors},
                     }
                 ],
                 'layout': {
-                    'xaxis': {'automargin': True},
+                    'xaxis': {
+                        'automargin': True,
+                    },
                     'yaxis': {
                         'automargin': True,
-                        'title': {'text': column}
+                        'title': {'text': 'Likes'}
                     },
                     'height': 250,
                     'margin': {'t': 10, 'l': 10, 'r': 10},
                 },
             },
         )
-        # check if column exists - user may have deleted it
-        # If `column.deletable=False`, then you don't
-        # need to do this check.
-        for column in ['count'] if column in dff
     ]
 
 if __name__ == '__main__':
