f94ebee8246bb737c6e40c0e0347c421afbfeb4e
pedr0vict0r <pedrovictorcosta7@gmail.com>
2021-02-08 23:27:05 -0300

    [WIP] Desmembramento do app.py

--- app.py
+++ app.py
@@ -3,73 +3,19 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 import dash
-from dash.dependencies import Input, Output, State
 import dash_table
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-import os
-import tweepy
-import logging
 
-import core.coletar_dados as core_cd
-import core.processar_dados as core_pd
-
-# carregar variaveis de ambiente do .env caso exista
-from dotenv import load_dotenv
-load_dotenv()
-
-# inicializa api do twitter
-auth = tweepy.OAuthHandler(os.environ['API_KEY'], os.environ['API_SECRET_KEY'])
-auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
-api = tweepy.API(auth)
+from metatags import metatags
 
 # inicializando dataframe
 df = pd.DataFrame(columns=['user', 'num_likes', 'num_replies', 'num_retweets', 'score'])
 
 # inicializa uma aplicacao em Dash
-app = dash.Dash(__name__, title='Interaciômetro | GP2W', external_stylesheets=[dbc.themes.BOOTSTRAP], meta_tags=[{
-    'http-equiv': 'X-UA-Compatible',
-    'content': 'IE=edge'
-}, {
-    'name': 'viewport',
-    'content': 'width=device-width, initial-scale=1.0'
-}, {
-    'name': 'description',
-    'content': 'Um medidor de interação de perfis no Twitter.'
-}, {
-    'property': 'og:url',
-    'content': 'https://interaciometro.herokuapp.com/'
-}, {
-    'property': 'og:site_name',
-    'content': 'Interaciômetro | GP2W'
-}, {
-    'property': 'og:type',
-    'content': 'website'
-}, {
-    'property': 'og:image',
-    'content': 'https://interaciometro.herokuapp.com/assets/logo.png'
-}, {
-    'property': 'og:title',
-    'content': 'Interaciômetro | GP2W'
-}, {
-    'property': 'og:description',
-    'content': 'Um medidor de interação de perfis no Twitter.'
-}, {
-    'property': 'twitter:title',
-    'content': 'Interaciômetro | GP2W'
-}, {
-    'property': 'twitter:description',
-    'content': 'Um medidor de interação de perfis no Twitter.'
-}, {
-    'property': 'twitter:image',
-    'content': 'https://interaciometro.herokuapp.com/assets/logo.png'
-}, {
-    'property': 'twitter:card',
-    'content': 'summary_large_image'
-}
-])
+app = dash.Dash(__name__, title='Interaciômetro | GP2W', external_stylesheets=[dbc.themes.BOOTSTRAP], meta_tags=metatags)
 
 server = app.server # expor variavel server para o Procfile
 
@@ -117,115 +63,7 @@ app.layout = html.Div(dbc.Container([
 #className="p-5"
 ))
 
-@app.callback(
-    [Output('the-alert', 'children'), Output('datatable-row-ids', 'data')],
-    [Input('submit-button', 'n_clicks')],
-    [State('user-input', 'value')])
-def update_username(n_clicks, username):
-    # reseta dataframe
-    df = pd.DataFrame(columns=['user', 'num_likes'])
-    retornos = [dash.no_update, dash.no_update]
-
-    if username != '':
-
-        tweets = core_cd.get_tweets(api=api, username=username)
-
-        if(tweets is not None):
-            likes = core_cd.get_likes(api=api, username=username)
-
-            likes_df = core_pd.top_users_likes(likes=likes)
-
-            replies_df = core_pd.top_users_replies(tweets=tweets)
-            # tratamento para retirar o próprio usuário das replies
-            index = replies_df['user'] == username
-            replies_df = replies_df.drop(replies_df.index[index])
-
-            retweets_df = core_pd.top_users_retweets(tweets=tweets)
-
-            df = core_pd.score(likes_df, replies_df, retweets_df)
-
-            # filtrar scores maiores que 10
-            index = pd.to_numeric(df['score']) > 10.0
-            df = df[index]
-
-            retornos = [dash.no_update, df.to_dict('records')]
-        else:
-            logging.info(f'Token Inválido ou Expirado.')
-            retornos = [alert, dash.no_update]
-
-    return retornos
-
-@app.callback(
-    Output('datatable-row-ids-container', 'children'),
-    [Input('datatable-row-ids', 'derived_virtual_data')])
-def update_graphs(rows):
-
-    dff = pd.DataFrame(data=rows, columns=['user', 'num_likes', 'num_replies', 'num_retweets', 'score'])
-
-    return [
-        # gráfico com likes, replies e retweets
-        dcc.Graph(
-            id='interações',
-            figure={
-                'data': [
-                    {
-                        'x': dff['user'],
-                        'y': dff['num_likes'],
-                        'type': 'bar',
-                        'name': 'Likes'
-                    },
-                    {
-                        'x': dff['user'],
-                        'y': dff['num_replies'],
-                        'type': 'bar',
-                        'name': 'Replies'
-                    },
-                    {
-                        'x': dff['user'],
-                        'y': dff['num_retweets'],
-                        'type': 'bar',
-                        'name': 'Retweets'
-                    }
-                ],
-                'layout': {
-                    'xaxis': {
-                        'automargin': True,
-                    },
-                    'yaxis': {
-                        'automargin': True,
-                        'title': {'text': 'interações'}
-                    },
-                    'height': 250,
-                    'margin': {'t': 10, 'l': 10, 'r': 10}
-                },
-            },
-        ),
-        # gráfico com o score de interação
-        dcc.Graph(
-            id='score',
-            figure={
-                'data': [
-                    {
-                        'x': dff['user'],
-                        'y': dff['score'],
-                        'type': 'bar'
-                    }
-                ],
-                'layout': {
-                    'xaxis': {
-                        'automargin': True,
-                    },
-                    'yaxis': {
-                        'automargin': True,
-                        'title': {'text': 'score'}
-                    },
-                    'height': 250,
-                    'margin': {'t': 10, 'l': 10, 'r': 10}
-                },
-            },
-        )
-        # for column in ['num_likes', 'num_replies', 'num_retweets', 'score'] if column in dff
-    ]
+from callbacks import *
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ callbacks.py
@@ -0,0 +1,120 @@
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import pandas as pd
+import logging
+
+from app import app, df, alert
+from api import api
+from core import coletar_dados as core_cd, processar_dados as core_pd
+
+@app.callback(
+    [Output('the-alert', 'children'), Output('datatable-row-ids', 'data')],
+    [Input('submit-button', 'n_clicks'), Input('user-input', 'n_submit')],
+    [State('user-input', 'value')])
+def update_username(n_clicks, n_submit, username):
+
+    # reseta dataframe
+    df = pd.DataFrame(columns=['user', 'num_likes'])
+    retornos = [dash.no_update, dash.no_update]
+
+    if username != '':
+
+        tweets = core_cd.get_tweets(api=api, username=username)
+
+        if(tweets is not None):
+            likes = core_cd.get_likes(api=api, username=username)
+
+            likes_df = core_pd.top_users_likes(likes=likes)
+
+            replies_df = core_pd.top_users_replies(tweets=tweets)
+            # tratamento para retirar o próprio usuário das replies
+            index = replies_df['user'] == username
+            replies_df = replies_df.drop(replies_df.index[index])
+
+            retweets_df = core_pd.top_users_retweets(tweets=tweets)
+
+            df = core_pd.score(likes_df, replies_df, retweets_df)
+
+            # filtrar scores maiores que 10
+            index = pd.to_numeric(df['score']) > 10.0
+            df = df[index]
+
+            retornos = [dash.no_update, df.to_dict('records')]
+        else:
+            logging.info(f'Token Inválido ou Expirado.')
+            retornos = [alert, dash.no_update]
+
+    return retornos
+
+@app.callback(
+    Output('datatable-row-ids-container', 'children'),
+    [Input('datatable-row-ids', 'derived_virtual_data')])
+def update_graphs(rows):
+
+    dff = pd.DataFrame(data=rows, columns=['user', 'num_likes', 'num_replies', 'num_retweets', 'score'])
+
+    return [
+        # gráfico com likes, replies e retweets
+        dcc.Graph(
+            id='interações',
+            figure={
+                'data': [
+                    {
+                        'x': dff['user'],
+                        'y': dff['num_likes'],
+                        'type': 'bar',
+                        'name': 'Likes'
+                    },
+                    {
+                        'x': dff['user'],
+                        'y': dff['num_replies'],
+                        'type': 'bar',
+                        'name': 'Replies'
+                    },
+                    {
+                        'x': dff['user'],
+                        'y': dff['num_retweets'],
+                        'type': 'bar',
+                        'name': 'Retweets'
+                    }
+                ],
+                'layout': {
+                    'xaxis': {
+                        'automargin': True,
+                    },
+                    'yaxis': {
+                        'automargin': True,
+                        'title': {'text': 'interações'}
+                    },
+                    'height': 250,
+                    'margin': {'t': 10, 'l': 10, 'r': 10}
+                },
+            },
+        ),
+        # gráfico com o score de interação
+        dcc.Graph(
+            id='score',
+            figure={
+                'data': [
+                    {
+                        'x': dff['user'],
+                        'y': dff['score'],
+                        'type': 'bar'
+                    }
+                ],
+                'layout': {
+                    'xaxis': {
+                        'automargin': True,
+                    },
+                    'yaxis': {
+                        'automargin': True,
+                        'title': {'text': 'score'}
+                    },
+                    'height': 250,
+                    'margin': {'t': 10, 'l': 10, 'r': 10}
+                },
+            },
+        )
+        # for column in ['num_likes', 'num_replies', 'num_retweets', 'score'] if column in dff
+    ]
